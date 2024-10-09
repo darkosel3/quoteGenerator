b33d278008a2b593eaaee25f28aad65d9023a228
darkosel3 <darkosel7@gmail.com>
2024-10-09 13:13:33 +0200

    hash router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {useState,useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { HashRouter } from "react-router-dom";
 
 
 
@@ -66,9 +67,11 @@ function QuoteBox(){
 
 function App() {
   return (
+    <HashRouter base='/'>
     <div className="App easeTransition">
       <QuoteBox />
     </div>
+    </HashRouter>
   );
 }
 
